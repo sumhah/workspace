32be86f47346b19f41781ae61bc07fb8d2e5563b
lbl <245237019@qq.com>
2017-12-27 22:57:53 +0800

    ts仅增加类型约束，差异不大， 可考虑迁移

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <div class="GameScreen">
+            <canvas width="512" height="448" ref="canvas">A drawing of something.</canvas>
+        </div>
+        <ul class="command">
+            <li class="left"></li>
+            <li class="right"></li>
+            <li class="up"></li>
+            <li class="down"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+
+    export default {
+        name: 'app',
+        mounted() {
+            const app = new App(this.$refs.canvas);
+        }
+    }
+</script>
+
+<style src="./scss/index.scss"></style>
